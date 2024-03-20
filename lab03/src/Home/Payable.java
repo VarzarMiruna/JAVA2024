42c4be3a6ee0f78d5fc2cc497db5f50084363c85
VarzarMiruna <125951638+VarzarMiruna@users.noreply.github.com>
2024-03-20 19:25:37 +0200

    Delete lab03/src/Home directory

--- lab03/src/Home/Payable.java
+++
@@ -1,6 +0,0 @@
-package Home;
-
-public interface Payable {
-    double getTicketPrice();
-}
-
--- lab03/src/Home/TimeInterval.java
+++
@@ -1,9 +0,0 @@
-package Home;
-
-import java.time.LocalTime;
-
-class TimeInterval extends Pair<LocalTime, LocalTime> {
-    public TimeInterval(LocalTime openingTime, LocalTime closingTime) {
-        super(openingTime, closingTime);
-    }
-}
